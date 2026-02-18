dee16993230f78454b53f2c77c3da28178d22969
Kirill Andrianov <kirill.2014196@gmail.com>
2026-02-18 18:46:32 +0700

    first commit

---
+++ GameNum/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameNum
+{
+    public class Player
+    {
+        public string name;
+        public int age;
+        public int health;
+
+        public Player() {
+            name = "TEST";
+            age = 1;
+            health = 3;
+        }
+        public Player(string name, int age, int health)
+        {
+            this.name = name;
+            this.age = age;
+            this.health = health;
+        }
+        public void Print()
+        {
+            Console.WriteLine(name + age + health);
+        }
+    }
+}
---
+++ GameNum/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameNum
+{
+    class Program
+    {
+        static bool NewGame()
+        {
+            Console.WriteLine("Хотите еще сыграть? Y N");
+
+            string choice = Console.ReadLine().ToUpper();
+            if (choice == "Y")
+                return false;
+            else
+                return true;
+            
+        }
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Компьютер загадывает число от 1 до 10");
+        }
+        static void DisplayMessageWin()
+        {
+            Console.WriteLine("Вы угадали число!!!");
+        }
+        static int CheckGuess(int guess, int secretNumber)
+        {
+            if (guess == secretNumber)
+                return 0;
+            else if (guess > secretNumber)
+                return 1;
+            else
+                return -1;
+        }
+        static bool ValidateGuess(int guessNum, int min, int max)
+        {
+            if (guessNum >= min && guessNum <= max)
+                return true;
+            else
+                return false;
+        }
+        
+        static int GenerateRandomNumber(int min, int max)
+        {
+            Random random = new Random();
+
+            int number = random.Next(min,max);
+
+            return number;
+        }
+        static int GetPlayerGuess()
+        {
+            try
+            {
+                Console.Write("Введите число: ");
+
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                return number;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка ввода. {e.Message}");
+
+                return -1;
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+
+            DisplayMessage();
+
+            int min = 1, max = 10, points = 0;
+            int randomNum =  GenerateRandomNumber(min,max);
+
+            int guessNum = GetPlayerGuess();
+            
+            bool flag = false;
+            while(!flag)
+            {
+                //guessNum = GetPlayerGuess();
+                bool res = ValidateGuess(guessNum, min, max);
+
+                if (res == false)
+                {
+                    Console.WriteLine("Ваше число за диапазоном!");
+                    guessNum = GetPlayerGuess();
+                }
+                else
+                {
+                    int result = CheckGuess(guessNum, randomNum);
+
+                    if (result == 0)
+                    {
+                        DisplayMessageWin();
+
+                        points++;
+                        Console.WriteLine($"Количество баллов: {points}");
+
+                        flag = NewGame();
+
+                    }
+                    else
+                    {
+                        points--;
+
+                        Console.WriteLine($"У вас осталось баллов: {points}");
+                        //if (points <= 0) { break; }
+                       
+                        guessNum = GetPlayerGuess();
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
